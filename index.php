8ee7e1d95d4609737e398c989ccd42e8f25f5853
elisatheng <elisa.theng@gmail.com>
2018-10-04 15:59:45 +0200

    listing : content of current directory done

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$currentFolderPath = dirname($_SERVER["SCRIPT_NAME"]);
+	require_once("fileindexer.class.php");
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +11,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="Elisa Theng"/>
 	
-	<link rel="stylesheet" href="<?php echo $currentFolderPath; ?>/assets/css/bootstrap.min.css"/>
-	<script src="<?php echo $currentFolderPath; ?>/assets/js/jquery-3.3.1.min.js"></script>
+	<link rel="stylesheet" href="<?php echo $FileIndexer->_path["project"]; ?>/assets/css/bootstrap.min.css"/>
+	<script src="<?php echo $FileIndexer->_path["project"]; ?>/assets/js/jquery-3.3.1.min.js"></script>
 
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Rammetto+One" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 <body>
-	
+	<div>
+		<h2>List content</h2>
+		<?php $FileIndexer->listContent(); ?>
+	</div>
 </body>
 </html>
