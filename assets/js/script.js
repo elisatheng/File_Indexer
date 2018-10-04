7531510592b94cb3c63268aee3a10d6215056778
elisatheng <elisa.theng@gmail.com>
2018-10-04 17:25:26 +0200

    front : tablesorter js lib activated

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,11 @@ $(function() {
 	$(".sidebar .sidebar-tree > li").children().children().setFolderOpenIcon();
 	$(".sidebar .sidebar-tree span").listSubfoldersOnClick();
 
+
+	/**
+	* TABLESORTER
+	* @plugin
+	*/
+	$(".body .body-section .tablesorter").tablesorter();
+
 });
